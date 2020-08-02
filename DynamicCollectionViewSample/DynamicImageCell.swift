7277755d2d330c2478f50b57b51cc0b656ce731e
Pankaj Sahni <pankajsahni@fico.com>
2020-08-02 19:22:31 +0530

    dynamic text cell created images of different sizes added

---
+++ DynamicCollectionViewSample/DynamicImageCell.swift
@@ -0,0 +1,18 @@
+//
+//  DynamicImageCell.swift
+//  DynamicCollectionViewSample
+//
+//  Created by pankajsahni on 02/08/20.
+//  Copyright © 2020 pankajsahni. All rights reserved.
+//
+
+import UIKit
+
+class DynamicImageCell: UICollectionViewCell {
+    @IBOutlet weak var dynamicImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
--- DynamicCollectionViewSample/ViewController.swift
+++ DynamicCollectionViewSample/ViewController.swift
@@ -11,18 +11,20 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
-    @IBOutlet weak var collectionLayout: UICollectionViewFlowLayout! {
-        didSet {
-            collectionLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
-        }
-    }
-    
     var itemsArr = ["Hi I am very good","Hi I am very good Hi I am very good Hi I am very good","Hi I am very good Hi I am very good Hi I am very good Hi I am very good", "Hi I am very good  Hi I am very good Hi I am very good Hi I am very good Hi I am very good Hi I am very good Hi I am very good"]
+    
+   
+   var imageArr = [UIImage(named: "10262165_stock-photo-holiday-burning-multicolored-candles-of-different-height"),UIImage(named: "five-stacks-coins-different-height-white-background-84931965"),UIImage(named: "images-2"),UIImage(named: "images"),UIImage(named: "stockfresh_id4966754_1d01b6")]
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        collectionView.register(UINib.init(nibName: "DynamicImageCell", bundle: nil), forCellWithReuseIdentifier: "DynamicImageCell")
         collectionView.register(UINib.init(nibName: "DynamicCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "DynamicCollectionViewCell")
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+                   flowLayout.estimatedItemSize = CGSize(width: 1,height: 1)
+         }
        
         collectionView.reloadData()
         // Do any additional setup after loading the view.
@@ -36,10 +38,23 @@ extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DynamicCollectionViewCell", for: indexPath) as! DynamicCollectionViewCell
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DynamicImageCell", for: indexPath) as! DynamicImageCell
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DynamicCollectionViewCell", for: indexPath) as! DynamicCollectionViewCell
+       // cell.dynamicImageView.image = imageArr[indexPath.row]
         cell.textLabel.text = itemsArr[indexPath.row]
         return cell
     }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+           return 0
+       }
+       func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+           return 0
+       }
     
     
     
@@ -78,5 +93,5 @@ extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource {
 //
 //        return size
 //    }
-}
+
 
