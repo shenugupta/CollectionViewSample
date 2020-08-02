8c84b347eb7a53647cb9f3c81ddf7efc698f4754
Pankaj Sahni <pankajsahni@fico.com>
2020-08-02 10:50:10 +0530

    first commit

---
+++ DynamicCollectionViewSample/DynamicCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DynamicCollectionViewCell.swift
+//  DynamicCollectionViewSample
+//
+//  Created by pankajsahni on 01/08/20.
+//  Copyright © 2020 pankajsahni. All rights reserved.
+//
+
+import UIKit
+
+class DynamicCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
